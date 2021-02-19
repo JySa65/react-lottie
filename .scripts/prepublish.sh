eeb991fd8a6822a11804ff935efadc6fe2558e19
Junior Sanchez <jysa65.dev@gmail.com>
2021-02-18 20:52:22 -0400

    fux bundle

--- .scripts/prepublish.sh
+++ .scripts/prepublish.sh
@@ -2,7 +2,6 @@
 echo "Starting build"
 echo ""
 rm -Rfv dist &
-esbuild "./src/index.jsx" --bundle --define:process.env.NODE_ENV=\"production\" --minify --outfile="./dist/index.js"
-# NODE_ENV=production ./node_modules/.bin/babel --ignore "./src/assets","./src/components","./src/lotties" --plugins "@babel/plugin-transform-runtime" --presets "react-app" ./src --out-dir ./dist
+NODE_ENV=production webpack --mode production
 echo ""
 echo "Finish build"
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require("path");
+
+module.exports = {
+  mode: 'production',
+  entry: "./src/index.js", // Elegimos nuestro punto de entrada
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "index.js",
+  }, // Añadimos nuestro punto de salida
+  resolve: {
+    extensions: [".js", ".jsx"], // Añadimos el soporte para la extencion de JSX
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/, // Ignora la carpeta de node_modules
+        use: {
+          loader: "babel-loader", // Utiliza la configuracion de Babel
+        },
+      },
+    ],
+  },
+};
